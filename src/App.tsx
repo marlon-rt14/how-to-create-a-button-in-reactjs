aff4b04b161dfaec45eb032c61f56730dbecbade
marlon-rt14 <mjavierrt14@gmail.com>
2024-03-10 21:08:10 -0500

    created video

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Boton } from "./components/Boton";
+import { BsFillPlayCircleFill } from "react-icons/bs";
 
 function App() {
   return (
     <div className="container">
-      <Boton >Hola mundo</Boton>
-      <Boton style={{backgroundColor: "red"}} >Hola mundo</Boton>
-      <Boton >Hola mundo</Boton>
+      <Boton>Click me!</Boton>
+      <Boton style={{ backgroundColor: "red" }}>Click me!</Boton>
+      {/* bootstrap classes */}
+      <Boton className="bg bg-success rounded rounded-5">Click me!</Boton>
+      <Boton className="d-flex justify-content-evenly">
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+      </Boton>
+      <Boton className="bg bg-warning">
+        <BsFillPlayCircleFill size={30} />
+      </Boton>
     </div>
   );
 }
 
 export default App;
-
